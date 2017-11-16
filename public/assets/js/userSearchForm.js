7c2260faea875e53691d7c1bf919f27ede385013
Laura Boetcher <g33klaura@gmail.com>
2017-11-16 14:28:57 -0500

    Initial commit for Laura's branch

---
+++ public/assets/js/userSearchForm.js
@@ -0,0 +1,19 @@
+// JavaScript controlling user inputs from search form for project 2
+// <script type="text/javascript" src="assets/js/userSearchForm.js"></script>
+
+/* Questions:
+  [] Link to index or other page (guessing other page, but discuss with team)
+*/
+
+// VARIABLES
+// ==========================================
+
+
+
+// FUNCTIONS
+// ==========================================
+
+
+
+// MAIN PROCESS
+// ==========================================
