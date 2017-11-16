9aa722976ae4adad023837a439e0432bb1f90fd3
Jessica Lin <jessicalin@Jessicas-MacBook-Pro.local>
2017-11-16 11:46:52 -0500

    new table update

--- models/ufo.js
+++ models/ufo.js
@@ -1,7 +1,16 @@
 module.exports = function (sequelize, DataTypes) {
     var UFO = sequelize.define("UFO", {
-        text: DataTypes.STRING,
-        complete: DataTypes.BOOLEAN
+        datetime: DataType.STRING,
+        city: DataTypes.STRING,
+        state: DataTypes.STRING,
+        country: DataTypes.STRING,
+        shape: DataTypes.STRING,
+        duration_sec: DataTypes.INTEGER,
+        duration_hours: DataTypes.INTEGER,
+        comments: DataTypes.STRING,
+        dateposted: DataTypes.INTEGER,
+        latitude: DataTypes.INTEGER,
+        longitude: DataTypes.INTEGER
     });
     return UFO;
 };
