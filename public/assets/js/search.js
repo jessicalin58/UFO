fe5a0bfaf8a9173c35c99a5eae037f41be217eed
Laura Boetcher <g33klaura@gmail.com>
2017-11-17 16:29:17 -0500

    Form renders text entries on page

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,76 +1,73 @@
 // JavaScript controlling user inputs from search form for project 2
 // <script type="text/javascript" src="assets/js/search.js"></script>
 
-/* Questions/needs:
-  [] Link to index or other page (guessing other page, but discuss with team)
-  [] jQuery link in html
+/* Todo:
+  [] input posts to page with button
+  [] button changes data-state up/down
 */
 
 // VARIABLES
 // ==========================================
 
+// Will store form input values for testing
+let sightingArray = [];
 
 
 // FUNCTIONS
 // ==========================================
 
+// Renders each user-submitted sighting to page
+function renderSightings() { 
+  // Empties div before adding new entries (so no entries are repeated)
+  $('#sightings').empty();
+
+  // Looping through array of topics
+  for (var i = 0; i < sightingArray.length; i++) {
+
+    // TEST: inputs log  ~WORKS
+    console.log(sightingArray[i]);
+
+    // Dynamically generate p-tag for each entry in array
+    var a = $('<p>').text(sightingArray[i]);
+
+    // Add Materialize button class
+    // a.addClass('waves-effect waves-light btn #8c9eff indigo accent-1');
+
+    // Add a class to each entry
+    a.addClass('sighting');
+
+    // Add data-attribute with value of the topics at index 'i'
+    // a.attr('data-name', topics[i]);
+    // Provide text for button based on string value of topics array at index 'i'
+    // a.text(topics[i]);
+    
+    $('#sightings').append(a);
+  };
+}
 
 
 // MAIN PROCESS
 // ==========================================
 
-// Contains click functions to add burger and devour burger
-
 $(function () {
-
-  $('.change-eat').on('click', function (event) {
-
-    console.log('devoured clicked');
-
-    let id = $(this).data('id');
-    let newEat = $(this).data('neweat');
-
-    let newEatState = {
-      devoured: newEat
-    };
-
-    // Send the PUT request
-    $.ajax('/api/UFO/' + id, {
-      type: 'PUT',
-      data: newEatState
-    }).then(
-      function () {
-        console.log('changed eat-status to', newEat);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-      );
-  });
-
-
-  // Click function for '#add-burger' button
-  $('#add-burger').on('click', function (event) {
-
-    console.log('submit clicked');
-
-    // Make sure to preventDefault on a submit event
+  
+  // Click function for 'add-sighting' submit button
+  $('#add-sighting').on('click', function (event) {
+    // console.log('add-sighting clicked');
     event.preventDefault();
 
-    let newBurger = {
-      comments: $('#new-burger').val().trim(),
-      // devoured: 0
-    };
+    // Need to render each element onto page
+    // Will be part of POST call later
+    let newSighting = $('#new-sighting').val().trim();
 
-    // Send the POST request
-    $.ajax('/api/UFO', {
-      type: 'POST',
-      data: newBurger
-    }).then(
-      function () {
-        console.log('created new delicious sighting log');
-        // Reload the page to get the updated list
-        location.reload();
-      }
-      );
+    // Adds user input to empty array
+    sightingArray.push(newSighting);
+
+    // Calls function to render each entry on page
+    renderSightings();
+
+    // Empties textbox input
+    $('#new-sighting').val('');
   });
-});  //Closes document-ready
+
+});
