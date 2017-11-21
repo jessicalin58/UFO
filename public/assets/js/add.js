7e66c2a10dee30df87f65c69c7ddaf2619d3d177
Laura Boetcher <g33klaura@gmail.com>
2017-11-20 20:16:42 -0500

    Clean up code

--- public/assets/js/add.js
+++ public/assets/js/add.js
@@ -1,9 +1,10 @@
-// JavaScript controlling user inputs from search form for project 2
-// <script type="text/javascript" src="assets/js/search.js"></script>
+// JavaScript controlling user-submitted entries (comments) from text input
+// <script type="text/javascript" src="assets/js/add.js"></script>
 
 /* Todo:
-  [] input posts to page with button
-  [] button changes data-state up/down
+  [x] input posts to page with button
+  [] store new entries in db
+  [] store total "ranking" (number of times each entry has been voted on)
 
   Questions:
   [] each new sighting report needs 'comments', 'date', what else?
@@ -14,12 +15,15 @@
 
 // Will store form input values for testing
 let sightingArray = [];
+
 // Set incremental data-id for each new entry
-let dataId = 0;
+// let dataId = 0;
 
 let upcount = 0;
 let downcount = 0;
-let totalCount = 0;
+
+// Will keep track of number of times each entry has been up/down voted
+// let totalCount = 0;
 
 
 // FUNCTIONS
@@ -34,7 +38,7 @@ function renderSightings() {
   for (var i = 0; i < sightingArray.length; i++) {
 
     // TEST: inputs log  ~WORKS
-    console.log(sightingArray[i].comments + ', ' + sightingArray[i].datetime);
+    // console.log(sightingArray[i].comments + ', ' + sightingArray[i].datetime);
 
     // Dynamically generate p-tag for each entry in array
     var p = $('<p>').text(sightingArray[i].comments + ' ' + sightingArray[i].datetime);
@@ -43,32 +47,28 @@ function renderSightings() {
     p.addClass('sighting upvote');
     p.attr('id', 'topic');
 
-    // Buttons that aren't part of upvote package structure
-    // Change upvote a-tags to buttons for now***************
+    // Buttons for up/down voting  (aren't part of upvote package structure)
     var upvoteBtn = $('<button>').addClass('upvote waves-effect waves-light btn indigo accent-1');
-    // Needs click function to incress upcount...
-    upvoteBtn.attr('data-upcount');
-    // upvoteBtn.text('Vote up');
-    // Add icon to upvote button
-    var upIcon = $('<i>').text('add');
-    upIcon.addClass('material-icons');
-    upvoteBtn.append(upIcon);
+      upvoteBtn.attr('data-upcount');
+      // Add icon to upvote button
+      var upIcon = $('<i>').text('add');
+      upIcon.addClass('material-icons');
+      upvoteBtn.append(upIcon);
     // Add upvote button to each entry
     p.append(upvoteBtn);
 
+    // Creates disabled input to act as vote-counter for each entry
     var counter = $('<input disabled value=0>').val(0);
-    counter.addClass('center-align');
-    counter.attr('id', 'disabled');
+      counter.addClass('center-align');
+      counter.attr('id', 'disabled');
     p.append(counter);
 
     var downvoteBtn = $('<button>').addClass('downvote waves-effect waves-light btn indigo accent-1');
-    // Needs click function to incress upcount...
-    downvoteBtn.attr('data-downcount');
-    // downvoteBtn.text('Vote down');
-    // Add icon to downvote button
-    var downIcon = $('<i>').text('remove');
-    downIcon.addClass('material-icons');
-    downvoteBtn.append(downIcon);
+      downvoteBtn.attr('data-downcount');
+      // Add icon to downvote button
+      var downIcon = $('<i>').text('remove');
+      downIcon.addClass('material-icons');
+      downvoteBtn.append(downIcon);
     // Add downvote button to each entry
     p.append(downvoteBtn);
 
@@ -106,8 +106,7 @@ function handleUpvote() {
   var currentPost = $(this).parent()
   // console.log(currentPost);
 
-  // Grabs value of counter for each entry*****  ~WORKS!!!!!!!!
-  // var $counterVal = $('#disabled');  //How to select 'this'#disabled
+  // Grabs value of counter for each entry  ~WORKS!!!!!!!!
   var $counterVal = currentPost.children('#disabled');
   // console.log($counterVal);
   var upcount = $counterVal.val();
@@ -154,6 +153,8 @@ $(function () {
     let newSighting = {
       comments: $('#new-sighting').val().trim(),
       datetime: moment(Date.now()).format('YYYY/MM/DD hh:mm a')
+      // vote: (DON'T FORGET TO ADD A COMMA TO ONE ABOVE),
+      // count: 
     }
 
     // Adds user input to empty array
@@ -172,32 +173,10 @@ $(function () {
 
   // Increase value of 'upcount' on click
   $(this).on('click', 'button.upvote', handleUpvote);
-  // $(this).on('click', 'button.upvote', function () {
-    // console.log(this);
-    // console.log('upvote clicked');
-    
-    // Grabs value of counter for each entry
-    // var $counterVal = $('#disabled');
-    // var upcount = $counterVal.val();
-    // Increases counter value by one
-    // upcount++;
-    // Updates counter value on page
-    // $counterVal.val(upcount);
-  // });
 
   // Increase value of 'downcount' on click
   $(this).on('click', 'button.downvote', handleDownvote);
-  // $(this).on('click', 'button.downvote', function () {
-    // console.log(this);
-    // console.log('downvote clicked');
 
-    // // Grabs value of counter for each entry
-    // var $counterVal = $('#disabled');
-    // var downcount = $counterVal.val();
-    // // Decreases counter value by one
-    // downcount--;
-    // $counterVal.val(downcount);
-  // });
 
   // How to keep running count of up/down votes?
   // totalCount
